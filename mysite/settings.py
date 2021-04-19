3aa1c46c12f200473f0a4e0f1da321c170966bdf
parthishere <parthishere1234@gmail.com>
2021-04-20 01:44:27 +0530

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 SECRET_KEY = 'a_^ix3psyy%wwv1-7nk33%worht5$oq&agf=e65^3js@nq*9&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['stormy-hamlet-70851.herokuapp.com', '127.0.0.1']
 
